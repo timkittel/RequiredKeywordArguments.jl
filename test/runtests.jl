6e0228724f8019798970e751354f13daaed2c3ed
Tim Kittel <yoho@posteo.de>
2018-07-03 08:58:23 +0200

    added more tests to increase code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,3 +29,25 @@ try
 catch e
     @test isa(e, RequiredKeywordArgumentError)
 end
+
+k(;y=RequiredKeywordArgument("y", "I can't compute `k` without `y`.")) = 3*y
+try
+    k()
+catch e
+    @test isa(e, RequiredKeywordArgumentError)
+    @test e.reason == "I can't compute `k` without `y`."
+end
+l(;y=RequiredKeywordArgument("y", reason="I can't compute `l` without `y`.")) = 4*y
+try
+    l()
+catch e
+    @test isa(e, RequiredKeywordArgumentError)
+    @test e.reason == "I can't compute `l` without `y`."
+end
+m(;y=RKA("y", reason="I can't compute `m` without `y`.")) = 5*y
+try
+    m()
+catch e
+    @test isa(e, RequiredKeywordArgumentError)
+    @test e.reason == "I can't compute `m` without `y`."
+end
